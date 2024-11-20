a4c9024f999a607c133225a0b1e17e691fa465be
Ángel Aguero <angelagueror23@gmail.com>
2024-11-19 23:19:58 -0400

    init commit

--- src/components/shared/navbar/NavLinks.tsx
+++ src/components/shared/navbar/NavLinks.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 import {NavLink} from "react-router-dom";
-import {NavItem} from "../../../utils";
 
+interface Props {
+    link: string,
+    title: string,
+}
 
-export const NavLinks = ({link, icon, title}: NavItem): React.JSX.Element => {
+export const NavLinks = ({link,  title}: Props): React.JSX.Element => {
     return (
         <>
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import {Brands, FeatureGrid, ProductGrid} from "../../components";
 import { popularCelulares, recentCelulares} from "../../data/init";
 import {prepareProducts} from "../../utils";
+import React from "react";
 
 const prepareRecentProduct = prepareProducts(recentCelulares);
 const preparePopularProduct = prepareProducts(popularCelulares);
@@ -11,6 +12,8 @@ export  const Home = ():React.JSX.Element => {
             <section  className="w-full">
                 <FeatureGrid />
                 <ProductGrid title={"Nuevos Productos"} product={prepareRecentProduct} />
+                <ProductGrid title={"Celulares Populares"} product={preparePopularProduct} />
+
                 <Brands />
             </section>
         </>
